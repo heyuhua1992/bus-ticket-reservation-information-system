1321604eddf4372b3b73015fd8e96a7b8752a08c
HeYuhua <heyuhua1992@163.com>
2018-05-10 22:25:30 +0800

    加入了极验校验的服务器 "gt3-node-sdk" 这个文件夹

---
+++ gt3-node-sdk/demo/static/fullpage.js
@@ -0,0 +1,13 @@
+var Geetest = require('../../gt-sdk');
+
+var captcha = new Geetest({
+    // demo 自带的
+    // geetest_id: 'd84c7e8e5e27b68bc8e25c6da4556135',
+    // geetest_key: 'de0250bcc4a5e8e7b156637b301cfe77'
+
+    // 我的
+    geetest_id: 'fdd75a4e6691fcda337d4305f54451df',
+    geetest_key: '5e35e376e93de9584846e1a7f1c4e081'
+});
+
+module.exports = captcha;
